c80edf69b42fa3f648f4aa7667f0af6d8f79c340
Pallavi tiwari <120261965+pallavicode-17@users.noreply.github.com>
2025-11-16 14:25:52 +0530

    Update Newcollection.jsx

--- Frontend/src/Components/newcollection/Newcollection.jsx
+++ Frontend/src/Components/newcollection/Newcollection.jsx
@@ -1,28 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import './newcollection.css'
-// import new_collection from '../Assets/new_collections'
-import Item from '../Item/Item'
-const NewCollections = () => {
+import React, { useEffect, useState } from 'react';
+import './newcollection.css';
+import Item from '../Item/Item';
+import { API_URL } from "../config";   // <--- IMPORTANT
 
+const NewCollections = () => {
   const [new_collection, setNew_Collection] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:4000/newcollection')
-    .then(res => res.json())
-    .then(data => setNew_Collection(data));
-  },[])
+    fetch(`${API_URL}/newcollection`)   // <--- REPLACED LOCALHOST
+      .then(res => res.json())
+      .then(data => setNew_Collection(data))
+      .catch(err => console.error("Failed to load new collection:", err));
+  }, []);
 
   return (
     <div className='new-collections'>
-        <h1>RECENTLY ADDED</h1>
-        <hr />
-        <div className="collections">
-            {new_collection.map((item,i) => {
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-            })}
-        </div>
+      <h1>RECENTLY ADDED</h1>
+      <hr />
+      <div className="collections">
+        {new_collection.map((item, i) => (
+          <Item 
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default NewCollections
+export default NewCollections;
